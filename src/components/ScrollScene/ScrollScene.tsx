fac4af15d9fe0d22f7198bd3bb0072547773d1ce
NataliaZhydeikina <nata.iiinx88@gmail.com>
2023-03-16 02:03:02 +0200

    separate scroll to component

---
+++ src/components/ScrollScene/ScrollScene.tsx
@@ -0,0 +1,43 @@
+import { RefObject, useEffect, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useScroll, ScrollControlsState, useKeyboardControls } from "@react-three/drei";
+import { MathUtils, Scene } from "three";
+import Controls from "../../utils/controls";
+
+type Scroll = ScrollControlsState & { scroll: { current: number } }
+type Props = {
+  children: JSX.Element,
+};
+
+function ScrollScene({ children }: Props) {
+  const scroll = useScroll() as Scroll;
+  const { height: h } = useThree((state) => state.viewport)
+  const ref = useRef<Scene>();
+  const [sub, get] = useKeyboardControls<Controls>()
+
+  useEffect(() => {
+    scroll.scroll.current = 0.5;
+  });
+
+  useFrame((state, delta) => {
+    if (!ref.current) return;
+    let { up, down } = get();
+    if (up) {
+      scroll.scroll.current = Math.max(0, scroll.scroll.current - 0.05);
+    }
+    if (down) {
+      scroll.scroll.current = Math.min(1, scroll.scroll.current + 0.05);
+    }
+    let x = ref.current.position.y;
+    let y = scroll.scroll.current * (-h + 100) + (h / 2) - 50;
+    ref.current.position.y = MathUtils.damp(x, y, 8, delta);
+  });
+
+  return (
+    <scene ref={ref as RefObject<Scene>}>
+      {children}
+    </scene>
+  );
+}
+
+export default ScrollScene;
